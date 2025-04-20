0597f8564d90c5fe15920b7479ec41d4dd7049e1
leessang10 <leessang10@naver.com>
2025-04-20 13:52:23 +0900

    fix: Prisma 관련 파일 및 CommonModule에서 PrismaService 제거

--- packages/common/src/common.module.ts
+++ packages/common/src/common.module.ts
@@ -1,11 +1,6 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-
 @Global()
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
+@Module({})
 export class CommonModule {
   static forRoot() {
     return {
